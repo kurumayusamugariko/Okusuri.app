7513b90c1422d1f4615b095ee60896fc58bf6bff
kurumayumac <junmomo6780@gmail.com>
2024-04-21 03:13:39 +0900

    Initial Commit

---
+++ Okusuri.app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Okusuri.app
+//
+//  Created by light neon on 2024/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Okusuri.app/Okusuri_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Okusuri_appApp.swift
+//  Okusuri.app
+//
+//  Created by light neon on 2024/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Okusuri_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
