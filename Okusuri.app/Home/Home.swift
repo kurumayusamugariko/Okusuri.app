e10c490262169944e499fd7161f00d06e5b9e862
kurumayumac <junmomo6780@gmail.com>
2024-04-27 21:00:53 +0900

    ボタン編集

--- Okusuri.app/Home/Home.swift
+++ Okusuri.app/Home/Home.swift
@@ -12,21 +12,25 @@ struct Home: View {
         
         NavigationView {
             VStack {
-                Text("名前")
-                HStack {
-                    Text("誕生日")
-                    Text("血液型")
-                } .padding(.all)
+                VStack{
+                    Text("名前")
+                        .padding(/*@START_MENU_TOKEN@*/[.top, .leading, .trailing]/*@END_MENU_TOKEN@*/)
+                    HStack {
+                        Text("誕生日")
+                        Text("血液型")
+                    } .padding(.all)
+                }.padding(.all).background(Color(hue: 0.611, saturation: 0.444, brightness: 1.0)).cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                
                 
                 HStack {
                     
                     NavigationLink{ info_medicine()} label:{
                         Text("飲んでいるお薬")
-                    } .padding(.all, 10)
+                    } .padding(.all, 10).frame(width: /*@START_MENU_TOKEN@*/170.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/170.0/*@END_MENU_TOKEN@*/).background(Color(hue: 0.611, saturation: 0.444, brightness: 1.0)).cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
                     
                     NavigationLink{ Hospital()} label:{
                         Text("病院一覧")
-                    } .padding(.all, 10)
+                    } .padding(.all, 10).frame(width: /*@START_MENU_TOKEN@*/170.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/170.0/*@END_MENU_TOKEN@*/).background(Color(hue: 0.611, saturation: 0.444, brightness: 1.0)).cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
                 }
                 
                 
@@ -34,15 +38,24 @@ struct Home: View {
                     
                     NavigationLink{ Medical_history()} label:{
                         Text("病歴")
-                    } .padding(.all, 10)
+                    } .padding(.all, 10).frame(width: /*@START_MENU_TOKEN@*/170.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/170.0/*@END_MENU_TOKEN@*/).background(Color(hue: 0.611, saturation: 0.444, brightness: 1.0)).cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
                     
                     
                     
                     NavigationLink{ allergy()} label:{
                         Text("アレルギー一覧")
-                    } .padding(.all, 10)
-                }
-            }
+                    } .padding(.all, 10).frame(width: /*@START_MENU_TOKEN@*/170.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/170.0/*@END_MENU_TOKEN@*/).background(Color(hue: 0.611, saturation: 0.444, brightness: 1.0)).cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                }.padding(.bottom)
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                    Text("カスタマイズ")
+                        .multilineTextAlignment(.center)
+                        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        .frame(width: /*@START_MENU_TOKEN@*/200.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+                        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color(hue: 0.51, saturation: 0.357, brightness: 0.913)/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                })
+
+            }.foregroundColor(/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/)
             
         }
     }
